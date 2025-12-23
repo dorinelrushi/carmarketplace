ceb5b47beabd9fb4d08de27b34c4d379fd7c318a
dorinelrushi <rushidorinel@gmail.com>
2025-12-23 19:59:53 +0100

    first commit

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,27 +1,27 @@
+"use client"; //
 import { SignIn } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
-    const { isSignedIn } = useUser();
-    const router = useRouter();
+  const { isSignedIn } = useUser();
+  const router = useRouter();
 
-    // If the user is already signed in, redirect to buyer dashboard
-    if (isSignedIn) {
-        router.push("/dashboard/buyer");
-        return null;
-    }
+  // If the user is already signed in, redirect to buyer dashboard
+  if (isSignedIn) {
+    router.push("/dashboard/buyer");
+    return null;
+  }
 
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-black">
-            <SignIn />
-            <p className="mt-4 text-gray-400">
-                Don't have an account?{' '}
-                <a href="/sign-up" className="text-red-600 underline">
-                    Register here
-                </a>
-            </p>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <SignIn />
+      <p className="mt-4 text-gray-400">
+        Don't have an account?{" "}
+        <a href="/sign-up" className="text-red-600 underline">
+          Register here
+        </a>
+      </p>
+    </div>
+  );
 }
-
--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -1,9 +1,10 @@
+"use client"; //
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-black">
-            <SignUp />
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <SignUp />
+    </div>
+  );
 }
