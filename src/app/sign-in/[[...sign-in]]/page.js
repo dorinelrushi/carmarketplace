cdaa5dd6d7afd16a7111ac980d616e58b46c9b63
dorinelrushi <rushidorinel@gmail.com>
2025-12-23 20:25:49 +0100

    first commit

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,7 +1,7 @@
 "use client"; //
 import { SignIn } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const { isSignedIn } = useUser();
